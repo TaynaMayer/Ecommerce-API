2a8a106ce04ecc00d963c3d2c1b876fefb7b5d88
Habner Phillippe <110646680+HabnerPhillippe@users.noreply.github.com>
2022-10-18 14:42:43 -0300

    atualização

--- src/main/java/br/com/serratec/controller/EnderecoController.java
+++ src/main/java/br/com/serratec/controller/EnderecoController.java
@@ -1,5 +1,43 @@
 package br.com.serratec.controller;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.serratec.exception.EmailException;
+import br.com.serratec.service.EnderecoService;
+
+
+@RestController
+@RequestMapping("/enderecos")
 public class EnderecoController {
+	@Autowired
+	private EnderecoService enderecoService;
+
+	@GetMapping
+	public ResponseEntity<List<EnderecoDTO>> listar() {
+		return ResponseEntity.ok(enderecoService.listar());
+	}
+
+	@PostMapping
+	public ResponseEntity<Object> inserir(@RequestBody EnderecoInserirDTO enderecoInserirDTO) {
+		try {
+			EnderecoDTO enderecoDTO = enderecoService.inserir(enderecoInserirDTO);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(enderecoDTO.getId()).toUri();
+			return ResponseEntity.created(uri).body(enderecoDTO);
+
+		} catch (EmailException e) {
+			return ResponseEntity.unprocessableEntity().body(e.getMessage());
+		}
+	}
 
 }
