f30da76cfe86a075ced2a76b9e68f6c0c6cd1f66
Tayná Mayer <112437100+TaynaMayer@users.noreply.github.com>
2022-10-18 14:44:25 -0300

    Incluindo service, repository, controller

--- src/main/java/br/com/serratec/service/PedidoService.java
+++ src/main/java/br/com/serratec/service/PedidoService.java
@@ -1,5 +1,62 @@
 package br.com.serratec.service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import br.com.serratec.exception.EmailException;
+
+
+@Service
 public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Autowired
+	private PedidoPerfilRepository pedidoPerfilRepository;
+	
+	@Autowired
+	private PerfilService perfilService;
+	
+	@Autowired
+	private MailConfig mailConfig;
+	
+	public List<PedidoDTO> listar() {
+		List<Pedido> pedidos = pedidoRepository.findAll();
+		List<PedidoDTO> pedidosDTO = new ArrayList<>();
+
+		for (Pedido pedido : pedidos) {
+			pedidosDTO.add(new PedidoDTO(pedido));
+
+		}
+		return pedidosDTO;
+		
+	}
+
+	public PedidoDTO inserir(PedidoInserirDTO pedidoInserirDTO) {
+		if (pedidoRepository.findByEmail(pedidoInserirDTO.getEmail()) != null) {
+			throw new EmailException("Email já existe na base");
+		}
+		Pedido pedido = new Pedido();
+		pedido.setNome(pedidoInserirDTO.getNome());
+		pedido.setEmail(pedidoInserirDTO.getEmail());
+		pedido.setSenha(bCryptPasswordEncoder.encode(pedidoInserirDTO.getSenha()));
+		pedido = pedidoRepository.save(pedido);
+		for (PedidoPerfil up : pedidoInserirDTO.getPedidosPerfil()) {
+			up.setPedido(pedido);
+			up.setPerfil(perfilService.buscar(up.getPerfil().getIdPerfil()));
+			up.setDataCriacao(LocalDate.now());			
+		}
+		pedidoPerfilRepository.saveAll(pedidoInserirDTO.getPedidosPerfil());		
+		mailConfig.sendEmail(pedido.getEmail(), "Cadastro de Usuário", pedido.toString());
+		return new PedidoDTO(pedido);
+	}
 
 }
