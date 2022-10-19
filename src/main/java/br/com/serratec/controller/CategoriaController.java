4d916d3aeb55c98dd4c8b1d2c6a9c14360ae0c5d
Habner Phillippe <110646680+HabnerPhillippe@users.noreply.github.com>
2022-10-19 15:12:35 -0300

    subindo dependencia pom

--- src/main/java/br/com/serratec/controller/CategoriaController.java
+++ src/main/java/br/com/serratec/controller/CategoriaController.java
@@ -1,49 +1,39 @@
 package br.com.serratec.controller;
 
-// import java.util.List;
-// import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
-// import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.util.UriComponents;
-// import org.springframework.web.util.UriComponentsBuilder;
+import br.com.serratec.dto.CategoriaRequestDTO;
+import br.com.serratec.dto.CategoriaResponseDTO;
+import br.com.serratec.service.CategoriaService;
 
-// import br.com.serratec.dto.CategoriaDTO;
-// import br.com.serratec.dto.CategoriaInserirDTO;
-// import br.com.serratec.model.Categoria;
-// import br.com.serratec.service.CategoriaService;
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
 
-
-
-// @RestController
-// @RequestMapping("/categorias")
-// public class CategoriaController {
-		
-// 		@Autowired
-// 		private CategoriaService categoriaService;
-		
-// 		@GetMapping
-// 		public ResponseEntity<List<CategoriaDTO>> buscarTodos() {
-// 			List<CategoriaDTO> listaCategorias = categoriaService.buscarTodos();
-// 			return ResponseEntity.ok(listaCategorias);
-// 		}
-		
-// 		@GetMapping("/{id}")
-// 		public ResponseEntity<Categoria> pesquisarUm(@PathVariable Long id) {
-// 			Optional<Categoria> categoria = categoriaService.buscarUm(id);
-// 			if (categoria.isPresent()) {
-// 				return ResponseEntity.ok(categoria.get());
-// 			}
-// 			return ResponseEntity.notFound().build();
-// 		}
-// }
+	@Autowired
+	private CategoriaService servico;
+	
+	@GetMapping
+	public List<CategoriaResponseDTO> obterTodos(){
+		return servico.obterTodos();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<CategoriaResponseDTO> oberPorId(@PathVariable Long id){
+		return  servico.obterPorId(id);
+	}
+	
+	@PostMapping
+	public CategoriaResponseDTO cadastrar(@RequestBody CategoriaRequestDTO categoria) {
+		return servico.cadastrar(categoria);
+	}
+}
--- src/main/java/br/com/serratec/service/CategoriaService.java
+++ src/main/java/br/com/serratec/service/CategoriaService.java
@@ -1,46 +1,65 @@
 package br.com.serratec.service;
 
-// import java.util.List;
-// import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-// import br.com.serratec.dto.CategoriaDTO;
-// import br.com.serratec.dto.CategoriaInserirDTO;
-// import br.com.serratec.model.Categoria;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import java.util.ArrayList;
+import br.com.serratec.dto.CategoriaRequestDTO;
+import br.com.serratec.dto.CategoriaResponseDTO;
+import br.com.serratec.model.Categoria;
+import br.com.serratec.repository.CategoriaRepository;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+@Service
+public class CategoriaService {
 
-// import br.com.serratec.repository.CategoriaRepository;
+	@Autowired
+	private CategoriaRepository repositorio;
+	
+	public List<CategoriaResponseDTO> obterTodos(){
+		
+		List<Categoria> categorias = repositorio.findAll();
+		ModelMapper mapper = new ModelMapper();
+		
+		// Aqui eu pega o lista de Categoria (Modelo), e converto um a uma em um CategoriaResponseDTO.
+		//No final devolvo a lista convertida.
+		return categorias.stream()
+				.map(categoria -> mapper.map(categoria, CategoriaResponseDTO.class))
+				.collect(Collectors.toList());
+		
+		
+	}
+	
+	public Optional<CategoriaResponseDTO> obterPorId(Long id){
+		
+		Optional<Categoria> optCategoria = repositorio.findById(id);
+		
+		if(optCategoria.isEmpty()) {
+			//Aqui lanço um exception
+			//Not found
+		}
+		
+		var categoriaDTO = new ModelMapper().map(optCategoria.get(), CategoriaResponseDTO.class);
+		
+		return Optional.of(categoriaDTO);
 
-// @Service
-// public class CategoriaService {
+	}
 
-//     @Autowired
-//     private CategoriaRepository categoriaRepository;
-    
-//     @Override
-//     public List<CategoriaDTO> encontrarTodos() {
-// 		List<Categoria> categorias = categoriaRepository.findAll();
-// 		List<CategoriaDTO> categoriasDTO = new ArrayList<CategoriaDTO>();
+    public CategoriaResponseDTO cadastrar(CategoriaRequestDTO categoriaDto) {
+		ModelMapper mapper = new ModelMapper();
+		
+		var categoriaModel = mapper.map(categoriaDto, Categoria.class);
+		
+		
+		categoriaModel.setIdCategoria(null);
+		categoriaModel =  repositorio.save(categoriaModel);
+		
+		return mapper.map(categoriaModel, CategoriaResponseDTO.class);
+		
+	}
 
-// 		for (Categoria categoria : categorias) {
-// 			CategoriaDTO categoriaDTO = new CategoriaDTO(categoria);
-// 			categoriasDTO.add(categoriaDTO);
-// 		}
 
-// 		return categoriasDTO;
-//     }
-
-//     @Override
-//     public List<Categoria> buscarTodos() {
-//         return categoriaRepository.findAll();
-//     }
-
-//     @Override
-//     public Optional<Categoria> buscarUm(Long id) {
-//         return categoriaRepository.findById(id);
-//     }
-
-  }
+}
