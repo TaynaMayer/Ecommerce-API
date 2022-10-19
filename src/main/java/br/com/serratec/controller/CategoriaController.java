368e379d286e837dfb639e8a6880cbc1c3f974eb
Habner Phillippe <110646680+HabnerPhillippe@users.noreply.github.com>
2022-10-19 11:57:48 -0300

    teste de categoria

--- src/main/java/br/com/serratec/controller/CategoriaController.java
+++ src/main/java/br/com/serratec/controller/CategoriaController.java
@@ -1,46 +1,49 @@
 package br.com.serratec.controller;
 
 // import java.util.List;
+// import java.util.Optional;
 
 // import javax.validation.Valid;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
 // import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.DeleteMapping;
 // import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
 // import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
 // import org.springframework.web.bind.annotation.RequestBody;
 // import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.ResponseStatus;
 // import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.util.UriComponents;
+// import org.springframework.web.util.UriComponentsBuilder;
 
+// import br.com.serratec.dto.CategoriaDTO;
+// import br.com.serratec.dto.CategoriaInserirDTO;
 // import br.com.serratec.model.Categoria;
 // import br.com.serratec.service.CategoriaService;
+
 
 
 // @RestController
 // @RequestMapping("/categorias")
 // public class CategoriaController {
-
-//     @Autowired
-//     private CategoriaService categoriaService;
-
-    // @GetMapping
-	// 	@ApiOperation(value = "Listar todas as categorias", notes = "Listagem com todas as categorias")
-	// 	@ApiResponses(value = {
-	// 			@ApiResponse(code = 200, message = "Retorna todas as categorias"),
-	// 			@ApiResponse(code = 401, message = "Erro de autenticação"),
-	// 			@ApiResponse(code = 403, message = "Você não tem permissão para acessar o recurso"),
-	// 			@ApiResponse(code = 404, message = "Recurso não encontrado"),
-	// 			@ApiResponse(code = 505, message = "Ocorreu alguma excessão")
-	// 	})
-
-    // public ResponseEntity<List<CategoriaDTO>> pesquisarTodos() {
-    //     List<CategoriaDTO> listaCategorias = categoriaService.pesquisarTodos();
-    //     return ResponseEntity.ok(listaCategorias);
-    // }
-
-
-    
-    
+		
+// 		@Autowired
+// 		private CategoriaService categoriaService;
+		
+// 		@GetMapping
+// 		public ResponseEntity<List<CategoriaDTO>> buscarTodos() {
+// 			List<CategoriaDTO> listaCategorias = categoriaService.buscarTodos();
+// 			return ResponseEntity.ok(listaCategorias);
+// 		}
+		
+// 		@GetMapping("/{id}")
+// 		public ResponseEntity<Categoria> pesquisarUm(@PathVariable Long id) {
+// 			Optional<Categoria> categoria = categoriaService.buscarUm(id);
+// 			if (categoria.isPresent()) {
+// 				return ResponseEntity.ok(categoria.get());
+// 			}
+// 			return ResponseEntity.notFound().build();
+// 		}
 // }
--- src/main/java/br/com/serratec/service/CategoriaService.java
+++ src/main/java/br/com/serratec/service/CategoriaService.java
@@ -1,10 +1,46 @@
 package br.com.serratec.service;
 
-import org.springframework.stereotype.Service;
+// import java.util.List;
+// import java.util.Optional;
 
-@Service
-public interface CategoriaService {
+// import br.com.serratec.dto.CategoriaDTO;
+// import br.com.serratec.dto.CategoriaInserirDTO;
+// import br.com.serratec.model.Categoria;
+
+// import java.util.ArrayList;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+
+// import br.com.serratec.repository.CategoriaRepository;
+
+// @Service
+// public class CategoriaService {
+
+//     @Autowired
+//     private CategoriaRepository categoriaRepository;
     
+//     @Override
+//     public List<CategoriaDTO> encontrarTodos() {
+// 		List<Categoria> categorias = categoriaRepository.findAll();
+// 		List<CategoriaDTO> categoriasDTO = new ArrayList<CategoriaDTO>();
 
-}
+// 		for (Categoria categoria : categorias) {
+// 			CategoriaDTO categoriaDTO = new CategoriaDTO(categoria);
+// 			categoriasDTO.add(categoriaDTO);
+// 		}
 
+// 		return categoriasDTO;
+//     }
+
+//     @Override
+//     public List<Categoria> buscarTodos() {
+//         return categoriaRepository.findAll();
+//     }
+
+//     @Override
+//     public Optional<Categoria> buscarUm(Long id) {
+//         return categoriaRepository.findById(id);
+//     }
+
+//  }
